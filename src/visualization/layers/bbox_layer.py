b0e3a9284a54e66268e3778434050ee20bbd037a
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-01-18 17:17:41 +0100

    Visualization Protocol refactor

--- src/visualization/layers/bbox_layer.py
+++ src/visualization/layers/bbox_layer.py
@@ -18,5 +18,24 @@ class BBoxLayer(Generic[T_BBox]):
         self.thickness = thickness
 
     def draw(self, data: T_BBox, image: np.ndarray) -> None:
-        bbox = data.bbox()
-        draw_rectangle(image, bbox[0], bbox[1], self.color, self.thickness)
+        """
+        Dibuja un bounding box sobre la imagen.
+        Convierte coordenadas normalizadas (0..1) a píxeles.
+        Se espera que `data` cumpla con el protocolo HasBBox:
+        bbox() -> ((x_min, y_min), (x_max, y_max)), normalizado
+        """
+        (x_min_norm, y_min_norm), (x_max_norm, y_max_norm) = data.bbox()
+
+        # Obtener tamaño de la imagen
+        height, width = image.shape[:2]
+
+        # Convertir coordenadas normalizadas a píxeles
+        x_min = int(round(x_min_norm * width))
+        y_min = int(round(y_min_norm * height))
+        x_max = int(round(x_max_norm * width))
+        y_max = int(round(y_max_norm * height))
+
+        # Dibujar el rectángulo usando coordenadas de píxel
+        draw_rectangle(
+            image, (x_min, y_min), (x_max, y_max), self.color, self.thickness
+        )
