aedb7de44db8562fcfb074a6ce8a1806538c9f19
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-01-18 15:54:10 +0100

    Reversed drawing order for sequence layer

--- src/visualization/visualizers/landmarks_sequence_visualizer.py
+++ src/visualization/visualizers/landmarks_sequence_visualizer.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Iterator, Sequence
+from typing import Iterable, Iterator
 import numpy as np
 
 from ..layers.sequence_layer import HasSequence
@@ -21,8 +21,9 @@ class LandmarksSequenceVisualizer(HasBBox, HasCentroid, HasPoints, HasSequence):
 	def points(self) -> np.ndarray:
 		return self._landmarks_sequence.array[-1]
 
-	def sequence(self) -> Sequence[LandmarksVisualizer]:
-		return [LandmarksVisualizer(landmarks) for landmarks in self._landmarks_sequence.landmarks]
+	def sequence(self) -> Iterator[LandmarksVisualizer]:
+		for landmarks in self._landmarks_sequence.landmarks:
+			yield LandmarksVisualizer(landmarks)
    
 	def time_stamps_ms(self) -> Iterable[int]:
 		return self._landmarks_sequence.time_stamps_ms
